155ba13799f02de4619ef3ef89746c79cb4e701d
Ritz GL <maurigarzonlara@hotmail.com>
2021-05-28 14:01:21 +0800

    implementing read/write file on server side

--- server.js
+++ server.js
@@ -8,6 +8,9 @@ const fs = require('fs');
 // Using the path of the 'public' directory
 app.use(express.static('./public'));
 
+// registering the express.json middleware
+app.use(express.json());
+
 // Direct user to the main page
 
 // /c/Bootcamp/note-taker/public/index.html
@@ -21,34 +24,41 @@ app.get('/api/notes', (req, res)=>{
     res.sendFile(path.join(__dirname,'db/db.json'))
 })
 
+app.post('/api/notes', async (req, res)=>{
+    const note = req.body;
+    
+    note.id = null; // set id onto note element
+    const notes = await getAllNotes(); // read from file all notes to creat array
+    notes.push(note); // push the newly created note unto the array
+    await writeNotes(notes); // write to file the entire array to update 
+    res.json(note); // respond to frontend with the newly created note
 
-// Direct user to homepage if no path matches
-app.get('*', (req, res) => res.sendFile(path.join(__dirname,'public/index.html')));
-
-
-app.post('/api/notes',(req, res, note)=>{
+    console.log(note);
     // read the json file to extract content
     fs.readFile('./db/db.json','utf8',(err, data)=>{
         if(err){
             console.error(err);
             return;
         }
-        console.log(data);
-        let objectsFromJSON = JSON.parse(data);
-        console.log(objectsFromJSON);
+        // console.log(data);
+        // let objectsFromJSON = JSON.parse(data);
+        // console.log(objectsFromJSON);
+        
+        // fs.writeFile('./db/db.json',)
     })
+
+
 })
 
 app.delete('/api/notes', ()=>{
 
 })
 
+// Direct user to homepage if no path matches
+app.get('*', (req, res) => res.sendFile(path.join(__dirname,'public/index.html')));
 
 // write to the JSON when the save button is pressed
-
-
 // identify the note user wants to delete via id
 // remove from the file
-
 
 app.listen(PORT, ()=> console.log(`App is listening on PORT ${PORT}`));
